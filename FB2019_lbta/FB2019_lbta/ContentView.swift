234a4a48273b31447afeab8a0febf1419d2cc0c8
kaiyuan zhao <zhao.kaiy@husky.neu.edu>
2019-12-13 00:17:25 -0500

    intial project

---
+++ FB2019_lbta/FB2019_lbta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FB2019_lbta
+//
+//  Created by 低调 on 12/13/19.
+//  Copyright © 2019 Kaiyuan Zhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
